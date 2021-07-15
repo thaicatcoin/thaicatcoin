00acdf4677783cfbe6d47e78ec4adce42c24ed77
thaicatcoin <87382705+thaicatcoin@users.noreply.github.com>
2021-07-15 15:48:11 +0200

    patched ascii error

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -22,7 +22,7 @@ const std::string nonWindowsAsciiArt =
     "   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═════╝ ╚═╝╚═╝  ╚═══╝\n";
 
 
-    
+
 /* Windows has some characters it won't display in a terminal. If your ascii
    art works fine on Windows and Linux terminals, just replace 'asciiArt' with
    the art itself, and remove these two #ifdefs and above ascii arts */
